8a9a3ef3ca008eba5ada91c326adbe888c8d0dae
DESKTOP-OODQB9L\Usman Khalid <usman.khalid@starzplayarabia.com>
2024-01-09 17:40:02 +0500

    adding test for my application and adding lombok in project

--- sprinInAction/src/main/java/com/example/sprinInAction/Controllers/HomeController.java
+++ sprinInAction/src/main/java/com/example/sprinInAction/Controllers/HomeController.java
@@ -1,6 +1,6 @@
 package com.example.sprinInAction.Controllers;
 
-import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
