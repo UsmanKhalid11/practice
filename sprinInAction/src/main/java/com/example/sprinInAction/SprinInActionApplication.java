e549cbd684790ab612bb0578cf75d89765869e89
DESKTOP-OODQB9L\Usman Khalid <usman.khalid@starzplayarabia.com>
2024-01-09 16:07:50 +0500

    spring in action chapter 1

---
+++ sprinInAction/src/main/java/com/example/sprinInAction/SprinInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinInAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinInActionApplication.class, args);
+	}
+
+}
---
+++ sprinInAction/src/test/java/com/example/sprinInAction/SprinInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinInAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
