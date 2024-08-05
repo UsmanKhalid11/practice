5f00987a06012ce51ee9c0b418a707190ec182f6
Usman Khalid <m.usman.khalid11@gmail.com>
2024-08-05 13:16:42 +0500

    using ... operator in java

--- designPatterns/designPatterns/src/Main.java
+++ designPatterns/designPatterns/src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        print("name","address");
+    }
+
+    public static void print(String ...name){
+        for(String item:name){
+            System.out.println(item);
+        }
     }
 }
